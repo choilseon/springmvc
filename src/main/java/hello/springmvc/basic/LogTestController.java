604cde8067e58a33f2121f028b1643cc7fc31562
choilseon <haha5959@naver.com>
2022-03-04 10:24:11 +0900

    로깅

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,24 @@
+package hello.springmvc.basic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // 문자열 return 하면
+public class LogTestController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        log.trace("trace log=" + name); // trace 가 출력이 되지 않을 때도 연산이 일어날 수 있음
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info("info log={}", name);
+        log.warn("warn log={}", name);
+        log.error("error log={}", name);
+        return "ok";
+    }
+}
